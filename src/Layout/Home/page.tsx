fa7b3c9fe706405ddd92cf4e4cf46f6b2e583417
AidanAli <aidanali65@gmail.com>
2024-12-01 00:18:39 -0500

    Designing HomePage

--- src/Layout/Home/page.tsx
+++ src/Layout/Home/page.tsx
@@ -1,30 +1,95 @@
-import {NavLink, Outlet} from "react-router-dom";
+import {
+    IconArrowWaveRightUp,
+    IconBoxAlignRightFilled,
+    IconBoxAlignTopLeft,
+    IconClipboardCopy,
+    IconFileBroken,
+    IconSignature,
+    IconTableColumn,
+} from "@tabler/icons-react";
+import {BentoGrid, BentoGridItem} from "@/components/ui/bento-grid.tsx";
+import {BackgroundBeamsWithCollision} from "@/components/ui/background-beams-with-collision.tsx";
 
-export const HomePage = () => {
+export function HomePage() {
     return (
         <>
-            <header className="text-4xl font-bold text-center justify-center">Home Page</header>
-            <div>
-                <ul>
-                    <li>
-                        <NavLink to='/guides'>Guides</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/event-timers'>Event Timers</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/event-guides'>Event Guides</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/class-guides'> Class Guides</NavLink>
-                    </li>
-                </ul>
-            </div>
-            <main>
-                <Outlet/>
-            </main>
+            <header
+                className="font-bold text-primary flex justify-center text-3xl sm:text-4xl py-4 sm:py-6 px-6 sm:px-8 my-4">
+                Welcome to Project Aurora
+            </header>
+            <BentoGrid className="max-w-4xl mx-auto">
+                {items.map((item, i) => (
+                    <BentoGridItem
+                        key={i}
+                        title={item.title}
+                        description={item.description}
+                        header={item.header}
+                        icon={item.icon}
+                        className={i === 3 || i === 6 ? "md:col-span-2" : ""}
+                    />
+                ))}
+            </BentoGrid>
+            <BackgroundBeamsWithCollision className="py-3 px-5 my-4">
+                <h2 className="text-2xl relative z-20 md:text-4xl lg:text-7xl font-bold text-center text-black dark:text-white font-sans tracking-tight">
+                    AuroraProject&apos;s Guides{" "}
+                    <div className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
+                        <div className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 py-4">
+                            <span className="">Official ArcheRage Wiki</span>
+                        </div>
+                    </div>
+                </h2>
+            </BackgroundBeamsWithCollision>
         </>
 
-
-    )
+    );
 }
+
+const Skeleton = () => (
+    <div
+        className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
+);
+const items = [
+    {
+        title: "New Player Guides",
+        description: "Explore the birth of groundbreaking ideas and inventions.",
+        header: <Skeleton />,
+        icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
+    },
+    {
+        title: "The Digital Revolution",
+        description: "Dive into the transformative power of technology.",
+        header: <Skeleton />,
+        icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
+    },
+    {
+        title: "The Art of Design",
+        description: "Discover the beauty of thoughtful and functional design.",
+        header: <Skeleton />,
+        icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
+    },
+    {
+        title: "The Power of Communication",
+        description:
+            "Understand the impact of effective communication in our lives.",
+        header: <Skeleton />,
+        icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
+    },
+    {
+        title: "The Pursuit of Knowledge",
+        description: "Join the quest for understanding and enlightenment.",
+        header: <Skeleton />,
+        icon: <IconArrowWaveRightUp className="h-4 w-4 text-neutral-500" />,
+    },
+    {
+        title: "The Joy of Creation",
+        description: "Experience the thrill of bringing ideas to life.",
+        header: <Skeleton />,
+        icon: <IconBoxAlignTopLeft className="h-4 w-4 text-neutral-500" />,
+    },
+    {
+        title: "The Spirit of Adventure",
+        description: "Embark on exciting journeys and thrilling discoveries.",
+        header: <Skeleton />,
+        icon: <IconBoxAlignRightFilled className="h-4 w-4 text-neutral-500" />,
+    },
+];
