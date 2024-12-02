9a24e3781d02b0344feba58cf2420c9c9344c639
AidanAli <aidanali65@gmail.com>
2024-12-02 01:43:51 -0500

    Home Page News Logic

--- src/Layout/News/page.tsx
+++ src/Layout/News/page.tsx
@@ -26,7 +26,7 @@ export default function WebsiteNews() {
                 <div className="mb-8 flex flex-col md:flex-row justify-between items-center">
                     <input
                         type="text"
-                        placeholder="Search games..."
+                        placeholder="Search News..."
                         className="w-full md:w-64 px-4 py-2 rounded-full bg-gray-300 dark:bg-gray-700 text-gray-800 dark:text-white placeholder-gray-500 dark:placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-purple-500 mb-4 md:mb-0"
                         value={searchTerm}
                         onChange={(e) => setSearchTerm(e.target.value)}
